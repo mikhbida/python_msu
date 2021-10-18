d6f847c84f542c062b55b40c68710ac80e13c936
mikhbida <92758514+mikhbida@users.noreply.github.com>
2021-10-19 00:03:36 +0300

    Update legacy.py

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -4,7 +4,13 @@ def t1(number):
     Пример: number=21 тогда нужно вернуть 40
     Пример: -5 -> 0
     """
-    pass
+    integ = number//20
+    if number%20 >0:
+        remain = 1
+    else:
+        remain = 0
+        
+    return (integ+remain)*20
 
 
 def t2(string):
@@ -12,21 +18,21 @@ def t2(string):
     На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
     Пример: `abc abc abc` -> `cba cba cba`
     """
-    pass
+    return string[::-1]
 
 
 def t3(dictionary):
     """
     На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
     """
-    pass
+    return str(dictionary).replace(',',';').replace('{','').replace('}','').replace("'","")
 
 
 def t4(string, sub_string):
     """
     проверить есть ли в строке инвертированная подстрока
     """
-    pass
+    return sub_string[::-1] in string
 
 
 def t5(strings):
@@ -34,4 +40,13 @@ def t5(strings):
     На вход подается список строк,
     Отфильтруйте список строк, оставив только строки в формате: `x y z x*y*z`, где x,y,z - целые положительные числа
     """
-    pass
+    ans = [int(s) for s in strings.split() if s.isdigit()]
+    
+    def multiplyList(myList):
+        
+        result = 1
+        for x in myList:
+             result = result * x
+        return result
+    
+    return multiplyList(ans)
