20bcf717920960c684b72cbbdb06f496f3651eac
mikhbida <92758514+mikhbida@users.noreply.github.com>
2021-10-19 00:02:58 +0300

    Update intro.py

--- year_2021/python/hw2/intro.py
+++ year_2021/python/hw2/intro.py
@@ -15,7 +15,8 @@ def reverse(lst: Optional[List[Any]]):
         [4, 3, 2, 1]
     ```
     """
-    return
+
+    return lst[::-1]
 
 
 def filter_by_indices(lst: Optional[List[Any]], indices: Optional[List[Any]]):
@@ -35,4 +36,9 @@ def filter_by_indices(lst: Optional[List[Any]], indices: Optional[List[Any]]):
         [3, 4]
     ```
     """
-    return
+    try:
+        for i in sorted(indices, reverse=True):
+            del lst[i]
+        return lst
+    except Exception as e:
+        return []
